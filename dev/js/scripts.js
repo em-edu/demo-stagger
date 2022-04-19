512be3e9139bb4c63970dea4023b3f0c53aec57f
em-edu <81432745+em-edu@users.noreply.github.com>
2022-04-19 17:59:27 -0400

    ready

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,25 +2,8 @@ import { gsap } from "gsap";
 
 //elements
 /*
-.red-box
-.blue-box
-.green-box
-
-rotation suffix and
-rotation:"180_short", transformOrigin:"0 0"
+.box
 */
 
-const mainTL = gsap.timeline({delay:2});
+//const mainTL = gsap.timeline();
 
-//gsap.to(".blue-box",{ duration:3, rotation:360, transformOrigin:"center", repeat:-1, ease:"none"});
-
-mainTL.to(".red-box",{duration:1, x:500, ease:"power1.out"}, "box")
-.to(".blue-box",{duration:1, x:500, ease:"power3.out"}, "box")
-.to(".green-box",{duration:1, x:500, ease:"back.inOut"}, "box")
-
-
-;
-
-mainTL.timeScale(0.25);
-
-//console.log("tl duration= "+tldur);
