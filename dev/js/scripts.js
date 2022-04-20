4c74b3c2ba3a978657a211abae720b1cf7e5dde6
em-edu <81432745+em-edu@users.noreply.github.com>
2022-04-19 20:28:46 -0400

    complete

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -5,5 +5,19 @@ import { gsap } from "gsap";
 .box
 */
 
-//const mainTL = gsap.timeline();
+const mainTL = gsap.timeline({delay:0.5});
 
+mainTL.from(".box", {duration:1, stagger:0.1, scale:0, transformOrigin:"center"})
+    
+    ;
+
+// mainTL.from(".box", {duration:1, 
+//     stagger:{
+//         each:0.1,
+//         from:"edges",
+//         grid:[3, 4],
+//         ease:"power1.inOut"
+//     },
+//      scale:0, transformOrigin:"center"})
+
+// ;
